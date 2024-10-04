cb302c95b5e9e23a228e9e1476c3be20a1be5353
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-04 11:38:57 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -42,35 +42,22 @@ if game.Players.LocalPlayer.leaderstats.Glove.Value == "ZZZZZZZ" then
 	game:GetService("ReplicatedStorage").nightmare:FireServer("LightBroken")
 	until game:GetService("BadgeService"):UserHasBadgeAsync(game.Players.LocalPlayer.UserId, 490455814138437)
 end
-wait(3)
+wait(20)
 local teleportFunc = queueonteleport or queue_on_teleport
-if teleportFunc then
-    teleportFunc([[
-        if not game:IsLoaded() then
-            game.Loaded:Wait()
+    if teleportFunc then
+        teleportFunc([[
+            if not game:IsLoaded() then
+                game.Loaded:Wait()
+            end
+            repeat wait() until game.Players.LocalPlayer
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-554, 177, 56)
+wait(0.7)
+for i,v in ipairs(game:GetService("Workspace"):GetDescendants()) do
+            if v.ClassName == "ProximityPrompt" then
+                fireproximityprompt(v)
+            end
         end
-        repeat wait() until game.Players.LocalPlayer
-        wait(3)
-Time = 121
-fireclickdetector(game.Workspace.CounterLever.ClickDetector)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0,100,0)
-wait(0.2)
-game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
-for i = 1,Time do
-Time = Time - 1
-game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "Wait [ "..Time.." ] seconds to get counter + elude glove.",Icon = "rbxassetid://7733658504",Duration = 1})
-wait(1)
-end
-game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
-wait(0.5)
-firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), game.Workspace.Ruins.Elude.Glove, 0)
-firetouchinterest(game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), game.Workspace.Ruins.Elude.Glove, 1)
-for i,v in pairs(workspace.Maze:GetDescendants()) do
-if v:IsA("ClickDetector") then
-fireclickdetector(v)
-end
-end
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/sus/main/main.lua'))()
-    ]])
+]])
 end
-game:GetService("TeleportService"):Teleport(11828384869)
+game:GetService("TeleportService"):Teleport(17290438723)
